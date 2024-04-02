648d2f3d2fa1e902eadc14e4f0b077077b8e643c
NiRovil <nicolas199851@hotmail.com>
2024-04-01 21:42:38 -0300

    Entidade User e seu resource

---
+++ src/main/java/com/projeto/curso/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.projeto.curso.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.projeto.curso.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Nome", "nome@nome.com", "222222", "1234");
+		return ResponseEntity.ok().body(u);
+	}
+	
+}
